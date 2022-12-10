3641c0c5f31e6d8a05dfe9cab564c22c781b72d5
Md Rakibul Hasan <rakibhasan0308@gmail.com>
2022-12-10 18:54:25 +0530

    Set home page and add asset

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import "./App.css";
+import Header from "./components/Header/Header.jsx";
+
 function App() {
   return (
-    <div className="App">
-      <h1>This is APP</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
