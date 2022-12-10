ebdaf0a5c9eb76b8b5385cc47f8880b05f8f4521
Md Rakibul Hasan <rakibhasan0308@gmail.com>
2022-12-10 20:03:56 +0530

    Complete nav bar with responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,62 @@
 import React from "react";
+import "./Header.css";
 
 const header = () => {
   return (
-    <div>
-      <h1>This is header</h1>
-    </div>
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">
+          Rakibul
+        </a>
+
+        <div className="nav__menu">
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link active-link">
+                <i className="uil uil-estate nav__icon"></i> Home
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon"></i> About
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#skilld" className="nav__link">
+                <i className="uil uil-file nav__icon"></i> Skills
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon"></i>
+                Services
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i> Portfolio
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav__icon"></i> Contact
+              </a>
+            </li>
+          </ul>
+
+          <i className="uil uil-times nav__close"></i>
+        </div>
+
+        <div className="nav__toggle">
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
   );
 };
 
